c56120018e23ffea3552e012acd7375223e760e1
Rxxbm <rubemcorrea0@gmail.com>
2023-08-17 03:02:34 -0300

    feat: create editUserPassword in UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,8 +1,11 @@
 import { Response, Request } from "express";
 import { BadRequestException } from "../exceptions/bad-request-exception";
 import { ReturnError } from "../exceptions/dto/return-error.dto";
+import { UserAuth } from "../models/dtos/user-auth.dto";
+import { UserEditPasswordDTO } from "../models/dtos/user-edit.dto";
 import { UserInsertDTO } from "../models/dtos/user-insert.dto";
-import { createUser, deleteUserById, getUsers } from "../services/user-services";
+import { createUser, deleteUserById, editUserPasswordById, getUsers } from "../services/user-services";
+import { getUserByToken } from "../utils/auth";
 
 export class UserController {
     public static async getAllUsers(__, res:Response):Promise<Response>{
@@ -37,6 +40,15 @@ export class UserController {
             }catch(error){
                 new ReturnError(res, error);
             }
+        }
+    }
+    public static async editUserPassword(req: Request<undefined, undefined, UserEditPasswordDTO>, res: Response):Promise<Response> {
+        try{
+            const userID = await getUserByToken(req).then((user: UserAuth) => user.userId);
+            const user = await editUserPasswordById(userID, req.body);
+            return res.status(200).json(user);
+        }catch(error){
+            new ReturnError(res, error);
         }
     }
 }
