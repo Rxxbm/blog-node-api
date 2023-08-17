e099c17be1869921843f492aac10be2ba64964f5
Rxxbm <rubemcorrea0@gmail.com>
2023-08-17 03:51:55 -0300

    feat: create getArticleById in article services and create article edit function

--- src/services/article-services.ts
+++ src/services/article-services.ts
@@ -1,6 +1,7 @@
 import { PrismaClient } from '@prisma/client'
 import { MissingParamException } from '../exceptions/missing-param-exception';
 import { NotFoundException } from '../exceptions/not-found-exception';
+import { UnauthorizedException } from '../exceptions/unauthorized-exception';
 import { Article } from '../models/Article';
 import { ArticleInsertDTO } from '../models/dtos/article-insert.dto';
 
@@ -36,4 +37,47 @@ export const createArticle = async (body:ArticleInsertDTO, userID: number):Promi
         data:article,
     });
     return userCreated;
+}
+
+export const getArticleById = async (id:number): Promise<Article> => {
+    const prisma = new PrismaClient();
+    const article = await prisma.article.findFirst({
+        where: {
+            id
+        }
+    });
+    if (!article) {
+        throw new NotFoundException('article');
+    }
+    return article;
+}
+
+export const editArticle = async (articleID:number, body: ArticleInsertDTO, userID: number):Promise<Article> => {
+    const prisma = new PrismaClient();
+
+    const requiredParams= ['title', 'body', 'category'];
+
+    for (const param of requiredParams) {
+        if (!body[param]) {
+            throw new MissingParamException(param);
+        }
+    }
+    const article = await getArticleById(articleID);
+
+    if (article.userID !== userID) {
+        throw new UnauthorizedException();
+    }
+    const newArticle: Article = {
+        ...article,
+        ...body
+    }
+
+    const editArticle = await prisma.article.update({
+        where: {
+          id: articleID,
+        },
+        data: newArticle,
+        
+    });
+    return editArticle;
 }
